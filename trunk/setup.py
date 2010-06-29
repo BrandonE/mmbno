858024232a43627360d7d7b80d913e1cdf76e610
faltzer <faltzer@84830149-9a12-4d01-9eeb-664e51bd8891>
2010-06-29 03:37:49 +0000

    Removed some old cruft; considering that the old Pyglet version can be run with just two files and won't interfere with the current structure, it has been moved to the project root.

---
+++ trunk/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import os
+
+script_file = 'game.py'
+
+data_files = []
+for file in os.listdir('mmbno'):
+    file = os.path.join('mmbno', file)
+    if os.path.exists(file):
+        data_files.append(file)
+
+setup_args = dict(
+    data_files=[('mmbno', data_files)]
+)
+
+try:
+    import py2exe
+    setup_args.update(dict(
+        windows=[dict(
+            script=script_file,
+            icon_resources=[(1, 'res/icon.ico')],
+        )],
+    ))
+except ImportError:
+    pass
+
+try:
+    import py2app
+    setup_args.update(dict(
+        app = [script_file],
+        options=dict(py2app=dict(
+            argv_emulation=True,
+            iconfile='res/icon.icns',
+        )),
+    ))
+except ImportError:
+    pass
+
+setup(**setup_args)
