ef2dd2b64c01f84e4b7ff5432866091c39e826a0
brandonevans <brandonevans@84830149-9a12-4d01-9eeb-664e51bd8891>
2010-08-12 00:52:45 +0000

    -I was going to let Chris make the 100th commit with the remainder of the panels, but I just realized I left one character of trailing whitespace in game.py. That's a HUGE security exploit, so I took immediate action.
    Happy 100th commit!

--- trunk/game.py
+++ trunk/game.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 # This file is part of MMBN Online
 # MMBN Online is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
