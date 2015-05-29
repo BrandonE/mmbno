3aba98ad6c504ad9b6675cee8523be98509dbef7
Brandon Evans <bevans@smartvue.com>
2015-05-29 09:32:10 -0500

    Create and draw the field.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var express = require('express'),
+var fs = require('fs'),
+    express = require('express'),
     app = express(),
+    config = JSON.parse(fs.readFileSync(__dirname + '/config.json')),
     http = require('http').Server(app),
     io = require('socket.io')(http),
-    socket_routes = require(__dirname + '/socket'),
-    fs = require('fs'),
+    socket = require(__dirname + '/socket')(config),
     server;
 
-app.set('config', JSON.parse(fs.readFileSync(__dirname + '/config.json')));
 app.set('views', __dirname, '../views');
 
 app.use(express.static(__dirname + '/../public'));
@@ -17,8 +17,8 @@ app.get('/', function(req, res) {
     res.sendFile('index.html', {root : __dirname + '/../views'});
 });
 
-socket_routes.attach(io);
+socket.attach(io);
 
-server = http.listen(app.get('config').port, function() {
+server = http.listen(config.port, function() {
     console.log('Express server listening on port ' + server.address().port);
 });
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,20 +1,113 @@
-var IO = null;
+var EOL = require('os').EOL;
 
-var connect = function (socket) {
-    IO.emit('user connected');
-    console.log('user connected');
-}
+module.exports = function(config) {
+    var IO = null,
+        field;
 
-var disconnect = function () {
-    IO.emit('user disconnected');
-    console.log('user disconnected');
-}
+    function connect(socket) {
+        IO.emit('user connected');
+        drawField();
+        console.log('user connected');
+    }
 
-exports.attach = function(io) {
-    IO = io;
+    function disconnect() {
+        IO.emit('user disconnected');
+        drawField();
+        console.log('user disconnected');
+    }
 
-    io.on('connection', function (socket) {
-        connect(socket);
-        socket.on('disconnect', disconnect);
-    });
+    function attach(io) {
+        IO = io;
+
+        io.on('connection', function (socket) {
+            if (!field) {
+                createField();
+            }
+
+            connect(socket);
+            socket.on('disconnect', disconnect);
+        });
+    };
+
+    return {
+        attach : attach
+    };
+
+    function createField() {
+        var row,
+            col,
+            cols,
+            panel;
+
+        if (config.rows < 1 || config.cols < 1 || config.cols % 2) {
+            throw new Error('Field dimensions invalid.');
+        }
+
+        field = [];
+
+        for (row = 0; row < config.rows; row++) {
+            cols = [];
+
+            for (col = 0; col < config.cols; col++) {
+                panel = {
+                    character : null,
+                    stolen    : false,
+                    status    : 'normal',
+                    time      : 0
+                };
+
+                cols.push(panel);
+            }
+
+            field.push(cols);
+        }
+    }
+
+    function drawField() {
+        var grid = EOL,
+            panelStatus = {
+                broken: 'B',
+                cracked: 'C',
+                grass: 'G',
+                holy: 'H',
+                ice: 'I',
+                lava: 'L',
+                normal: ' ',
+                metal: 'M',
+                poison: 'P',
+                sand: 'S',
+                water: 'W'
+            },
+            row,
+            col,
+            r,
+            c;
+
+        for (r in field) {
+            row = field[r];
+
+            grid += ' ----- ----- ----- ----- ----- -----' + EOL +
+                '|';
+
+            for (c in row) {
+                col = row[c];
+
+                grid += ' ' + panelStatus[col.status];
+
+                if (col.character && col.character.health) {
+                    grid += 'x';
+                } else {
+                    grid += ' ';
+                }
+
+                grid += '  |';
+            }
+
+            grid += EOL +
+                ' ----- ----- ----- ----- ----- -----' + EOL
+        }
+
+        console.log('\033[2J');
+        console.log(grid);
+    }
 };
