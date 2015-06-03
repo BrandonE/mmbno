5b08574f5fcc55b04501c7359c984e3018d3d357
Brandon Evans <bevans@smartvue.com>
2015-06-02 23:32:48 -0500

    Bad parameter order for extending.

--- server/chips/extend.js
+++ server/chips/extend.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function extend(Child, Parent, io, game, config, character) {
+module.exports = function extend(Child, Parent, io, config, game, character) {
     Child.prototype = new Parent(io, config, game, character);
     Child.constructor = Child;
     return new Child(io, config, game, character);
