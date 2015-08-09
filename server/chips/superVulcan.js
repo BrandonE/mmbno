88f19c6a690194c3347980220c7aaf0018b48579
Brandon Evans <admin@brandonevans.org>
2015-08-08 22:36:15 -0500

    Added super vulcan.

---
+++ server/chips/superVulcan.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var VulcanType = require(__dirname + '/types/vulcan'),
+    extend = require(__dirname + '/extend');
+
+function SuperVulcanConstructor(io, config, game, character) {
+    this.properties.codes = ['V'];
+    this.properties.description = '10-shot vulcan cannon';
+    this.properties.name = 'Super Vulcan';
+    this.properties.short = 'SuprVulc';
+    this.properties.bullets = 10;
+    this.properties.power = 20;
+    this.properties.stars = 4;
+}
+
+module.exports = function SuperVulcan(io, config, game, character) {
+    return extend(SuperVulcanConstructor, VulcanType, io, config, game, character);
+};
